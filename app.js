e882af92afdd7a557ef6c425b4b152d8ee413bca
praveen377 <55344902+praveen377@users.noreply.github.com>
2021-02-25 12:14:14 +0530

    level 4(5) seurity with salting

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+
+//this library is used for salting the data
+const bcrypt = require("bcrypt");
+// this specify how much round you want to salting.
+const saltRounds = 10;
 
 
 
@@ -42,26 +46,32 @@ app.get("/register",function(req,res){
 
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            res.render("secrets");
-        }
-    });
 
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                res.render("secrets");
+            }
+        });
+
+    });
+    
 })
 
 app.post("/login",function(req,res){
+    
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email:username},function(err,founduser){
 
         if(err)
@@ -71,10 +81,10 @@ app.post("/login",function(req,res){
         else
         {
             if(founduser){
-                if(founduser.password===password)
-                {
+                bcrypt.compare(password,founduser.password,function(error,result){
+                    if(result === true)
                     res.render("secrets");
-                }
+                });
             }
         }
 
